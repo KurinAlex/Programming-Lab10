87f1cd7e67ebb766f562db44190e7ed84e0dc4ca
Kurin, Alex <olexkurin@gmail.com>
2021-10-28 10:54:05 +0300

    lab10 algorithm improvement

--- Programming-Lab10/Programming-Lab10.c
+++ Programming-Lab10/Programming-Lab10.c
@@ -9,21 +9,43 @@ void EnterDouble(double* num, const char* num_name)
 
 void OutputSquareEquationRoots(double a, double b, double c, const char* variable_name)
 {
-	double D = b * b - 4 * a * c;
-
 	printf("Square equation %lf%s^2 + %lf%s + %lf = 0 ", a, variable_name, b, variable_name, c);
-	if (D < 0)
+
+	if (a == 0.0)
 	{
-		printf("don`t have real roots\n");
-	}
-	else if (D == 0)
-	{
-		printf("has one root: %lf\n", -b / (2 * a));
+		if (b == 0.0)
+		{
+			if (c == 0.0)
+			{
+				printf("has infinity roots");
+			}
+			else
+			{
+				printf("don't have roots");
+			}
+		}
+		else
+		{
+			printf("has one root: %lf", -c / b);
+		}
 	}
 	else
 	{
-		double sqrt_D = sqrt(D);
-		printf("has two roots: %lf and %lf\n", (-b + sqrt_D) / (2 * a), (-b - sqrt_D) / (2 * a));
+		double D = b * b - 4 * a * c;
+
+		if (D < 0.0)
+		{
+			printf("don`t have real roots\n");
+		}
+		else if (D == 0.0)
+		{
+			printf("has one root: %lf\n", -b / (2 * a));
+		}
+		else
+		{
+			double sqrt_D = sqrt(D);
+			printf("has two roots: %lf and %lf\n", (-b + sqrt_D) / (2 * a), (-b - sqrt_D) / (2 * a));
+		}
 	}
 }
 
